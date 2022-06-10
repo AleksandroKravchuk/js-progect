18b8e05b5960f116cc05ce540972f2bfebe1c922
Dmytro Yanko <dmytroyanko@MacBook-Pro.local>
2022-06-10 20:17:46 +0300

    start class

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 
 // const apiData = new ApiService();
 // console.log(apiData)
+import Header from './js/header';
 import getData from './js/api';
 console.log(getData())
+
+const header = new Header();
--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,38 @@
-export default headerRefs = {
-    homeBtn: document.querySelector('a[data-link="home"]'),
-    myLibraryBtn: document.querySelector('a[data-link="library"]'),
-    libraryBottons: document.querySelector('.librari-list'),
-}
+// export default headerRefs = {
+//     homeBtn: document.querySelector('a[data-link="home"]'),
+//     myLibraryBtn: document.querySelector('a[data-link="library"]'),
+//     libraryBottons: document.querySelector('.librari-list'),
+// }
 
-const { homeBtn, myLibraryBtn, libraryBottons } = headerRefs;
+// const { homeBtn, myLibraryBtn, libraryBottons } = headerRefs;
 
 // console.log('homeBtn: ', headerRefs.homeBtn);
 // console.log('myLibraryBtn: ', headerRefs.myLibraryBtn);
 
-class Header {
+export default class Header {
     refs = {
         homeBtn: document.querySelector('a[data-link="home"]'),
         myLibraryBtn: document.querySelector('a[data-link="library"]'),
         libraryBottons: document.querySelector('.librari-list'),
     }
 
-    
+    getRefs() {
+
+    }
+
+    showLibraryBtn() {
+
+    }
+
+    hideLibraryBtn() {
+
+    }
+
+    showForm() {
+
+    }
+
+    hideForm() {
+        
+    }
 } 
